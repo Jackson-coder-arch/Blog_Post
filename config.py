5b32a0c718da4ab981f8269ca409904349e8bd7b
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-03-08 14:57:21 +0300

    deployment

--- config.py
+++ config.py
@@ -1,51 +1,3 @@
-# import os
-
-
-
-# class Config:
-
-    
-#     SECRET_KEY = '12345'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jack:jack@localhost/blog'
-
-
-#     UPLOADED_PHOTOS_DEST ='app/static/photos'
-
-#     #  email configurations
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
-
-# class ProdConfig(Config):
-#     # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jack:jack@localhost/blog'
-
-#     SQLALCHEMY_DATABASE_URL=os.environ.get("DATABASE_URL")
-
-
-
-# class DevConfig(Config):
-#     # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jack:jack@localhost/blog'
-
-#     SQLALCHEMY_DATABASE_URL=os.environ.get("DATABASE_URL")
-
-#     DEBUG = True
-
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig
-# }
-
-
-
-
-
-
-
-
 import os
 
 from dotenv import load_dotenv as ld
@@ -63,7 +15,9 @@ class Config:
     # app.config['SECRET_KEY'] = SECRET_KEY
 
     debug = True
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jack:jack@localhost/blog'
+    # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jack:jack@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
 
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SECRET_KEY = os.environ.get('SECRET_KEY')
